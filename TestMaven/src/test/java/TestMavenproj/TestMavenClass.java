8bcb66cd4b02a4cf03bf65c329f4a36213f9d8e2
sumanmitra17 <suman.mitra@indusnet.co.in>
2018-12-17 13:29:20 +0530

    First

---
+++ TestMaven/src/test/java/TestMavenproj/TestMavenClass.java
@@ -0,0 +1,18 @@
+package TestMavenproj;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestMavenClass {
+
+	@Test
+	public void testHello()
+	{
+		System.out.println("Welcome to the World of Maven.........");
+		System.setProperty("webdriver.chrome.driver", "E:\\Suman\\Softwares\\Cell\\Drivers\\Chrome\\chromedriver.exe");
+		WebDriver dr = new ChromeDriver();
+		dr.get("https://www.google.com");
+		System.out.println("The page title is : " + dr.getTitle()+" got it man????");
+	}
+}
