2dde5b0e00da9cd0c28ed5d7406e035d73eb5731
sumanmitra17 <suman.mitra@indusnet.co.in>
2018-12-17 14:29:36 +0530

    adding failed one

--- TestMaven/src/test/java/TestMavenproj/TestMavenClass.java
+++ TestMaven/src/test/java/TestMavenproj/TestMavenClass.java
@@ -1,18 +1,26 @@
 package TestMavenproj;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestMavenClass {
 
-	@Test
-	public void testHello()
+	static WebDriver dr;
+	@Test(priority=0)
+	public static void testHello()
 	{
 		System.out.println("Welcome to the World of Maven.........");
 		System.setProperty("webdriver.chrome.driver", "E:\\Suman\\Softwares\\Cell\\Drivers\\Chrome\\chromedriver.exe");
-		WebDriver dr = new ChromeDriver();
+		dr = new ChromeDriver();
 		dr.get("https://www.google.com");
 		System.out.println("The page title is : " + dr.getTitle()+" really got it man........????");
+	}
+	
+	@Test(priority=1)
+	public static void failingTest() throws Exception
+	{
+		dr.findElement(By.name("sd")).click();
 	}
 }
